726f9a3217dcd6fd82359d6956b5758544f93efd
Thierry Boiago <58778448+thierryboiago@users.noreply.github.com>
2023-09-05 19:07:10 -0300

    Initial commit

---
+++ app/src/main/java/com/thierryboiago/walpaperapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.thierryboiago.walpaperapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
