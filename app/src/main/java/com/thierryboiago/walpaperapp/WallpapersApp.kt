cc54c8879771ebf8cb80ed9c577876380f4835ec
Thierry Boiago <58778448+thierryboiago@users.noreply.github.com>
2023-09-12 19:37:39 -0300

    add framework

---
+++ app/src/main/java/com/thierryboiago/walpaperapp/WallpapersApp.kt
@@ -0,0 +1,9 @@
+package com.thierryboiago.walpaperapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class WallpapersApp : Application() {
+
+}
---
+++ core/src/main/java/com/thierryboiago/core/MyClass.kt
@@ -0,0 +1,4 @@
+package com.thierryboiago.core
+
+class MyClass {
+}
